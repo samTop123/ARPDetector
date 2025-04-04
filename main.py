3bea367fb45567e48ad36494726b01945c4c7d7b
samTop123 <barsam618@gmail.com>
2025-04-04 21:21:40 +0300

    detect all the devices by getting the arp table

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import scapy.all as scapy
 import constants
 
-def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    
-    clients_list = []
-    for element in answered_list:
-        clients_list.append({"ip": element[1].psrc, "mac": element[1].hwsrc})
-    return clients_list
+def get_arp_table(ip_range):
+    """Scans the network and retrieves the ARP table."""
+    arp_request = scapy.ARP(pdst=ip_range)
+    ether_frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether_frame / arp_request
 
-def display_result(results):
-    print("IP Address\t\tMAC Address\n")
-    for client in results:
-        print(client["ip"] + "\t\t" + client["mac"])
+    answered, _ = scapy.srp(packet, timeout=2, verbose=0)
+
+    arp_table = {}
+    for _, received in answered:
+        ip = received.psrc
+        mac = received.hwsrc
+        arp_table[ip] = mac
+    return arp_table
 
 target_ip = constants.target_ip
-display_result(scan(target_ip))
+print(get_arp_table(target_ip))
